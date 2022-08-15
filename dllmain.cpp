c6c1c5f57a5a6ddf1aea6a607e21688d3a1bcf04
xan1242 <8014093+xan1242@users.noreply.github.com>
2022-08-15 17:35:28 +0200

    Add project files.

---
+++ dllmain.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include <windows.h>
+#include "includes\injector\injector.hpp"
+#include "includes\IniReader.h"
+
+int Init()
+{
+	injector::WriteMemory<unsigned int>(0x0042ABB8, 0x42AA3D, true);
+	injector::WriteMemory<unsigned int>(0x0042ABBC, 0x42AA3D, true);
+
+	return 0;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		Init();
+	}
+	return TRUE;
+}
+
